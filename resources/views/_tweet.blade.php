b6d90029000e9686aef212d564c1e691ab851928
robin <robinlanckman@hotmail.com>
2020-07-08 17:39:19 +0200

    Added individual tweet

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,15 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+
+
+<div>
+    <h5 class="font-bold mb-4">John Doe</h5>
+    <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet autem dicta illo ratione rerum. Consectetur cupiditate pariatur voluptatum. Blanditiis consectetur cum excepturi expedita minus numquam optio perferendis quidem repudiandae tempora?</p>
+</div>
+</div>
