21271dc9379e36419f4b776374be537470709a3a
robin lanckman <RobinLanckman@hotmail.com>
2020-07-09 16:21:55 +0200

    Added fake DB inserts. Dynamic view timeline user and tweet. Compact function to show authenticated user->avatar

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,7 +1,7 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-2 flex-shrink-0">
         <img
-            src="https://i.pravatar.cc/50"
+            src="https://i.pravatar.cc/50?u={{$tweet->user->email}}"
             alt=""
             class="rounded-full mr-2"
         >
@@ -9,7 +9,9 @@
 
 
 <div>
-    <h5 class="font-bold mb-4">John Doe</h5>
-    <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet autem dicta illo ratione rerum. Consectetur cupiditate pariatur voluptatum. Blanditiis consectetur cum excepturi expedita minus numquam optio perferendis quidem repudiandae tempora?</p>
+    <h5 class="font-bold mb-4">{{$tweet->user->name}}</h5>
+    <p class="text-sm">
+        {{$tweet->body}}
+    </p>
 </div>
 </div>
