81e169ab5d25f8b4feae67fefdff046e1ddce19d
robin lanckman <RobinLanckman@hotmail.com>
2020-07-09 14:58:56 +0200

    Added tweet model and controller

---
+++ database/factories/TweetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\tweet;
+use Faker\Generator as Faker;
+
+$factory->define(tweet::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
